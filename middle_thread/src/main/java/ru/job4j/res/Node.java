ef2dbca03922f95324a35123aeec66364f041108
stGOST <dda.90@mail.ru>
2021-07-12 21:31:13 +0500

    3. Immutable объекты [#267918]

---
+++ middle_thread/src/main/java/ru/job4j/res/Node.java
@@ -0,0 +1,27 @@
+package ru.job4j.res;
+
+public class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    //public void setNext(Node<T> next) {
+    //    this.next = next;
+    //}
+
+    public T getValue() {
+        return value;
+    }
+
+    //public void setValue(T value) {
+    //    this.value = value;
+    //}
+}
