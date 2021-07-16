d1b58f4d51495f08402fdcf861293b05a6ac5226
stGOST <dda.90@mail.ru>
2021-07-16 19:48:25 +0500

    0. CAS - операции [#6859 #178089]

--- middle_thread/src/main/java/ru/job4j/nonblock/CASCount.java
+++ middle_thread/src/main/java/ru/job4j/nonblock/CASCount.java
@@ -9,7 +9,12 @@ public class CASCount {
     private final AtomicReference<Integer> count = new AtomicReference<>();
 
     public void increment() {
-        count.compareAndSet(count.get(), count.get() + 1);
+        int i;
+        int iNext;
+        do {
+            i = count.get();
+            iNext = i + 1;
+        } while (!count.compareAndSet(i, iNext));
     }
 
     public int get() {
