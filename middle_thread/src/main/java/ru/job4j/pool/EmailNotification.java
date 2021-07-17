12e212412f1cfaa8ed955a2910c48774892dd8f5
stGOST <dda.90@mail.ru>
2021-07-17 12:05:30 +0500

    2. ExecutorService рассылка почты. [#63097]

---
+++ middle_thread/src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,63 @@
+package ru.job4j.pool;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class EmailNotification {
+    private ExecutorService service =
+            Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    //Public
+    public void emailTo(User user) {
+        service.submit(getRunnableSendEmailTo(user));
+    }
+
+    public  void close() {
+        service.shutdown();
+    }
+
+    //Private
+    private void send(String subject, String body, String email) {
+        System.out.printf("%s/%s/%s%n", email, subject, body);
+    }
+
+    private String getSubject(User user) {
+        return String.format("Notification %s to email %s.",
+                user.getUsername(),
+                user.getEmail());
+    }
+
+    private String getBody(User user) {
+        return String.format("Add a new event to %s",
+                user.getUsername());
+    }
+
+    private Runnable getRunnableSendEmailTo(User user) {
+        return () -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                send(getSubject(user), getBody(user), user.getEmail());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        };
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<User> users = new LinkedList<>();
+        users.add(new User("Dima", "dima@com.ru"));
+        users.add(new User("Stas", "stas@com.ru"));
+        users.add(new User("Petr", "petr@com.ru"));
+
+        var emailSender = new EmailNotification();
+
+        users.forEach(emailSender::emailTo);
+
+        emailSender.close();
+        //TimeUnit.SECONDS.sleep(2);
+        System.out.println("Email send");
+    }
+}
---
+++ middle_thread/src/main/java/ru/job4j/pool/User.java
@@ -0,0 +1,31 @@
+package ru.job4j.pool;
+
+public class User {
+    private final String username;
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "username='" + username + '\''
+                + ", email='" + email + '\''
+                + '}';
+    }
+}
