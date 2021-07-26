a5603bf0a7f31833fddfe023cbf8b0bd50d1174f
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-07-26 21:41:50 +0500

    asinc

--- middle_thread/src/main/java/ru/job4j/pool/asinc/RunAsyncExample.java
+++ middle_thread/src/main/java/ru/job4j/pool/asinc/RunAsyncExample.java
@@ -1,6 +1,9 @@
 package ru.job4j.pool.asinc;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class RunAsyncExample {
@@ -137,6 +140,38 @@ public class RunAsyncExample {
         System.out.println("Кто сейчас моет руки?");
         TimeUnit.SECONDS.sleep(1);
         System.out.println(first.get());
+    }
+
+    public static int[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+        int n = matrix.length;
+        int[] sums = new int[2 * n];
+        Map<Integer, CompletableFuture<Integer>> futures = new HashMap<>();
+        // считаем сумму по главной диагонали
+        futures.put(0, getTask(matrix, 0, n - 1, n - 1));
+        // считаем суммы по побочным диагоналям
+        for (int k = 1; k <= n; k++) {
+            futures.put(k, getTask(matrix, 0, k - 1,  k - 1));
+            if (k < n) {
+                futures.put(2 * n - k, getTask(matrix, n - k, n - 1, n - 1));
+            }
+        }
+        for (Integer key : futures.keySet()) {
+            sums[key] = futures.get(key).get();
+        }
+        return sums;
+    }
+
+    public static CompletableFuture<Integer> getTask(
+            int[][] data, int startRow, int endRow, int startCol) {
+        return CompletableFuture.supplyAsync(() -> {
+            int sum = 0;
+            int col = startCol;
+            for (int i = startRow; i <= endRow; i++) {
+                sum += data[i][col];
+                col--;
+            }
+            return sum;
+        });
     }
 
     public static void main(String[] args) throws Exception {
