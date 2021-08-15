7c147883695dee1a3cbc92c4df1776c361e17839
DDobrovolskiy <dda.90@mail.ru>
2021-08-15 21:43:39 +0500

    6. CORS [#305092]

---
+++ middle_servlet/src/main/java/ru/job4j/dream/filter/CORSFilter.java
@@ -0,0 +1,29 @@
+package ru.job4j.dream.filter;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORSFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest sreq, ServletResponse sresp, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) sreq;
+
+        ((HttpServletResponse) sreq).addHeader(
+                "Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) sreq).addHeader(
+                "Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
+
+        HttpServletResponse resp = (HttpServletResponse) sresp;
+
+        if (request.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+
+        chain.doFilter(request, sresp);
+    }
+}
