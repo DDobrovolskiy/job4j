e8906a45f4a895ca69a1ad1dae54ad74d70197e1
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-09-01 22:59:46 +0500

    1. Интеграционные тесты с Hqldb [#4746]

---
+++ trainee_all/src/test/java/ru/job4j/tracker/HbmTrackerTest.java
@@ -0,0 +1,66 @@
+package ru.job4j.tracker;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class HbmTrackerTest {
+    private final Store store = new  HbmTracker();
+
+    @Test
+    public void replace() {
+        Item item = new Item("test");
+        item = store.add(item);
+
+        String newName = "name";
+        store.replace(item.getId(), new Item(newName));
+
+        Item result = store.findById(item.getId());
+
+        Assert.assertThat(result.getId(), is(item.getId()));
+        Assert.assertThat(result.getName(), is(newName));
+    }
+
+    @Test
+    public void delete() {
+        String name = "name";
+        Item item = new Item(name);
+        item = store.add(item);
+
+        Item resultNotNull = store.findById(item.getId());
+
+        Assert.assertThat(resultNotNull.getName(), is(name));
+
+        store.delete(item.getId());
+
+        Item resultNull = store.findById(item.getId());
+
+        Assert.assertNull(resultNull);
+    }
+
+    @Test
+    public void findAll() {
+        String name = "name";
+        Item item = new Item(name);
+        store.add(item);
+
+        var result = store.findAll();
+
+        Assert.assertThat(result.size(), is(1));
+        Assert.assertThat(result.get(0).getName(), is(name));
+    }
+
+    @Test
+    public void findByName() {
+        String name = "name";
+        Item item = new Item(name);
+        store.add(item);
+
+        var result = store.findByName(name);
+
+        Assert.assertThat(result.size(), is(1));
+        Assert.assertThat(result.get(0).getName(), is(name));
+    }
+}
